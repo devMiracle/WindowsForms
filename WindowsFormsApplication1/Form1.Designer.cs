b0a0bed2911ea647ab2f6cdcd9edaba095fb2d68
Miracle <rjcvjchzljv9@gmail.com>
2020-02-05 11:50:09 +0200

    1

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -28,12 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.okButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // okButton
+            // 
+            this.okButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.okButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.okButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.okButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.PaleTurquoise;
+            this.okButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.okButton.Font = new System.Drawing.Font("Showcard Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.okButton.ForeColor = System.Drawing.Color.Black;
+            this.okButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.okButton.Location = new System.Drawing.Point(140, 12);
+            this.okButton.Name = "okButton";
+            this.okButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.okButton.Size = new System.Drawing.Size(64, 64);
+            this.okButton.TabIndex = 1;
+            this.okButton.Text = "ok";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            this.okButton.MouseDown += new System.Windows.Forms.MouseEventHandler(this.okButton_MouseDown);
+            this.okButton.MouseEnter += new System.EventHandler(this.okButton_MouseEnter);
+            this.okButton.MouseLeave += new System.EventHandler(this.okButton_MouseLeave);
+            this.okButton.MouseHover += new System.EventHandler(this.okButton_MouseHover);
+            this.okButton.MouseMove += new System.Windows.Forms.MouseEventHandler(this.okButton_MouseMove);
+            this.okButton.MouseUp += new System.Windows.Forms.MouseEventHandler(this.okButton_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.cancelButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.cancelButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.cancelButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.PaleTurquoise;
+            this.cancelButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cancelButton.Font = new System.Drawing.Font("Showcard Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelButton.ForeColor = System.Drawing.Color.Black;
+            this.cancelButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.cancelButton.Location = new System.Drawing.Point(76, 82);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.cancelButton.Size = new System.Drawing.Size(128, 64);
+            this.cancelButton.TabIndex = 3;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            this.cancelButton.MouseDown += new System.Windows.Forms.MouseEventHandler(this.cancelButton_MouseDown);
+            this.cancelButton.MouseEnter += new System.EventHandler(this.cancelButton_MouseEnter);
+            this.cancelButton.MouseLeave += new System.EventHandler(this.cancelButton_MouseLeave);
+            this.cancelButton.MouseMove += new System.Windows.Forms.MouseEventHandler(this.cancelButton_MouseMove);
+            this.cancelButton.MouseUp += new System.Windows.Forms.MouseEventHandler(this.cancelButton_MouseUp);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(169, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.unp;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(216, 204);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.okButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -16,5 +16,80 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void okButton_MouseEnter(object sender, EventArgs e)
+        {
+            label1.Text = "Кнопка Ok";
+        }
+
+        private void okButton_MouseLeave(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            label2.Text = "";
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void cancelButton_MouseEnter(object sender, EventArgs e)
+        {
+            label1.Text = "Кнопка Cancel";
+        }
+
+        private void cancelButton_MouseLeave(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            label2.Text = "";
+        }
+
+        private void okButton_MouseDown(object sender, MouseEventArgs e)
+        {
+            label1.Text = "Click!";
+        }
+
+        private void cancelButton_MouseDown(object sender, MouseEventArgs e)
+        {
+            label1.Text = "Click!";
+        }
+
+        private void okButton_MouseUp(object sender, MouseEventArgs e)
+        {
+            okButton_MouseEnter(sender, e);
+        }
+
+        private void cancelButton_MouseUp(object sender, MouseEventArgs e)
+        {
+            cancelButton_MouseEnter(sender, e);
+        }
+
+        private void okButton_MouseMove(object sender, MouseEventArgs e)
+        {
+            label2.Text = "X: " + e.X.ToString() + " Y: " + e.Y.ToString();
+        }
+
+        private void cancelButton_MouseMove(object sender, MouseEventArgs e)
+        {
+            label2.Text = "X: " + e.X.ToString() + " Y: " + e.Y.ToString();
+        }
+
+        private void okButton_MouseHover(object sender, EventArgs e)
+        {
+            label3.Text = "test";
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
