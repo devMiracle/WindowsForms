cb121c49b6071a6d232efe22d2301c5e3bc270d7
Miracle <rjcvjchzljv9@gmail.com>
2020-02-19 06:56:17 +0200

    added new project

---
+++ ApplicationTest/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ApplicationTest
+{
+    public partial class Form1 : Form
+    {
+        static String host = System.Net.Dns.GetHostName();
+        IPAddress ip = Dns.GetHostByName(host).AddressList[1];
+        
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            toolStripStatusLabel1.Text = ip.ToString();
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SendMessageButton_Click(object sender, EventArgs e)
+        {
+            MessageSender.SendMessage("test", "Hello World!");
+
+        }
+    }
+}
---
+++ ApplicationTest/MessageSender.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+using System.Net;
+using System.Net.Mail;
+using System.Windows.Forms;
+
+namespace ApplicationTest
+{
+    public static class MessageSender
+    {
+        //TODO: допилить
+        public static void SendMessage(string _subject, string _message)
+        {
+            SmtpClient _smtp = new SmtpClient("smtp.gmail.com", 587);
+
+            _smtp.Credentials = new NetworkCredential("username", "password");
+
+            _smtp.EnableSsl = true;
+
+            MailMessage _mail = new MailMessage();
+
+            _mail.From = new MailAddress("mail");
+
+            _mail.To.Add(new MailAddress("mail"));
+
+            _mail.SubjectEncoding = Encoding.UTF8;
+
+            _mail.BodyEncoding = Encoding.UTF8;
+
+            _mail.Subject = _subject;
+
+            _mail.Body = _message;
+
+            try
+            {
+                _smtp.Send(_mail);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+
+
+    }
+
+}
